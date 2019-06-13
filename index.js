22d7121db4802eac214469c46c39cec66df17a88
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-14 00:27:57 +0545

    Using http module for server

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 var express = require('express'),
-    path = require('path');
+    path = require('path'),
+    http = require('http');
 
 var app = express();
-var viewPath = path.join(__dirname, "views");
 
 app.set('port', 9000);
 app.use('/static', express.static(path.join(__dirname, 'assets')));
-customers = [{ id: 1, name: 'Pradip' },
-    { id: 2, name: 'Akash' }
-];
 
-app.get('/', (req, resp) => {
-    resp.sendFile(viewPath + '/index.html');
+var server = http.createServer(app);
+
+server.on('connection', (socket) => {
+    socket.on('connect', () => {
+        console.log('Connection request form %s', socket.remoteAddress);
+    })
 });
 
-app.get('/customers', (req, resp) => {
-    resp.setHeader('Content-Type', 'Application/json');
-    resp.send(customers);
+server.listen(app.get('port'), () => {
+    console.log('Server running at port %d', app.get('port'));
 });
-
-app.get('/customers/detail/:id', (req, resp) => {
-    resp.send('<h1>Customer Detail</h1>Customer Id: ' + req.params.id);
-});
-
-app.get('/colors', (req, resp) => {
-    resp.send(colors);
-});
-
-app.get('/about', (req, resp) => {
-    resp.redirect('/customers');
-});
-
-app.listen(app.get('port'), () => {
-    console.log('server running at port %d', app.get('port'));
-})
