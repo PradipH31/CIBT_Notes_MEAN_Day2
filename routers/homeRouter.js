c31bb3630fb16df42ce47a0211d5dac1947bb3bb
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-14 07:46:21 +0545

    Sending data from ejs and using it\

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -6,7 +6,10 @@ var router = express.Router();
 var colors = ['rec', 'green', 'blue'];
 
 router.get('/', (req, resp) => {
-    resp.render('index');
+    resp.render('index', {
+        name: 'Pradip ko laptop',
+        colors: colors
+    });
 });
 
 router.get('/colors', (req, resp) => {
