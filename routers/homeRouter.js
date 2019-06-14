f6d1e95e8f79243c80ab34bffc3255ec9c3e2eef
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-14 07:27:15 +0545

    Sending array through router

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -3,8 +3,14 @@ var express = require('express'),
 
 var router = express.Router();
 
+var colors = ['rec', 'green', 'blue'];
+
 router.get('/', (req, resp) => {
     resp.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+router.get('/colors', (req, resp) => {
+    resp.send(colors);
 });
 
 module.exports = router;
