f52c43ae53fe8d6b913638931c8c82152c6d0a58
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-16 15:29:57 +0545

    Using response to send query results

--- routers/eventRouter.js
+++ routers/eventRouter.js
@@ -1,9 +1,24 @@
 var express = require('express');
-
 var router = express.Router();
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: 'localhost',
+    database: 'crm',
+    user: 'root',
+    password: ''
+});
 
 router.get('/', (req, resp) => {
-    resp.send('<h1>Events</h1>');
+    var sql = 'select * from tbl_events';
+    db.query(sql, function(err, result) {
+        var content = '<ul>';
+        result.forEach((row, index) => {
+            content += '<li>' + row.event_name + '</li>';
+        });
+        content += '</ul>'
+        resp.send(content);
+    });
 });
 
 module.exports = router;
